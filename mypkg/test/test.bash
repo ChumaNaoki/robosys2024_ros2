45dd55e8add1f15dda80c59e8d7326365a5caeb5
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2025-01-02 22:05:19 +0900

    test

--- mypkg/test/test.bash
+++ mypkg/test/test.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+source /opt/ros/humble/setup.bash
+source /root/ros2_ws/install/setup.bash
+
 source ~/ros2_ws/install/setup.bash
 
 dir=~
 [ "$1" != "" ] && dir="$1"
+
+source /opt/ros/humble/setup.bash
+source /root/ros2_ws/install/setup.bash
+
 
 cd $dir/ros2_ws
 colcon build
